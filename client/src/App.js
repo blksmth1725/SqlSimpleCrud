a569e72b29c4ac29e8e3f82e8ef2bcf899e719db
Christian Sheen <csheen1725@gmail.com>
2022-01-05 18:35:12 -0500

    UseState use

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,62 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [postion, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const displayInfo = () => {
+    console.log({
+      name,
+      age,
+      country,
+      postion,
+      wage,
+    });
+  };
+
   return (
     <div className="App">
       <div className="information">
         <label>Name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
         <label>Age:</label>
-        <input type="text" />
+        <input
+          type="Number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
         <label>Country:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
         <label>Position:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
         <label>Wage (yearly):</label>
-        <input type="number" />
-        <button>Add Employee</button>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button onClick={displayInfo}>Add Employee</button>
       </div>
     </div>
   );
