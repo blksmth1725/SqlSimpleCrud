7dfa0b44e709f1ad574bc8dbe8d7198425d1dc66
Christian Sheen <csheen1725@gmail.com>
2022-01-04 22:26:25 -0500

    Messing with inputs

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <label>Name:</label>
-      <input type="text" />
-      <label>Age:</label>
-      <input type="text" />
-      <label>Country:</label>
-      <input type="text" />
-      <label>Position:</label>
-      <input type="text" />
-      <label>Wage (yearly):</label>
-      <input type="number" />
+      <div className="information">
+        <label>Name:</label>
+        <input type="text" />
+        <label>Age:</label>
+        <input type="text" />
+        <label>Country:</label>
+        <input type="text" />
+        <label>Position:</label>
+        <input type="text" />
+        <label>Wage (yearly):</label>
+        <input type="number" />
+      </div>
     </div>
   );
 }
